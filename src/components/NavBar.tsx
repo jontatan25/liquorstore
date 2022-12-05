ac40c4761c8197fe7d642836d2dc26de8254106c
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-12-04 23:54:38 -0500

    Feat: NavBar Ad implemented

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./style.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="navBar">
+        <div className="navBar__ad -flex -acenter -jcenter">
+          <h5 className="navBar__text -flex">
+            FREE SHIPPING on Wheat & Honey
+            <svg
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              height="18px"
+              width="20px"
+              viewBox="150 0 476.213 476.213"
+              xmlSpace="preserve"
+            >
+              <polygon
+                points="345.606,107.5 324.394,128.713 418.787,223.107 0,223.107 0,253.107 418.787,253.107 324.394,347.5 
+	345.606,368.713 476.213,238.106 "
+              />
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+              <g></g>
+            </svg>
+          </h5>
+          <button className="navBar__ad__close -btn-primary">
+          <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"/></svg>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
