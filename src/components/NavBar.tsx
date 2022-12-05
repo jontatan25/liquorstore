746c001c98dd5b901a0ab9753ea48dd5aeb62237
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-12-05 11:05:16 -0500

    Feat: NavList close Button implemented. Updated classes to reuse it.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -44,7 +44,7 @@ const NavBar = () => {
               <g></g>
             </svg>
           </h5>
-          <button className="navBar__ad__close -btn-primary">
+          <button className="navBar__close -btn-primary">
             <svg
               width="24px"
               height="24px"
@@ -66,8 +66,23 @@ const NavBar = () => {
             OUR WHISKEY
             <div className="item__arrow"></div>
           </button>
-          <div className="nav__menu -flex" aria-expanded={false}>
-            <div className="menu__left -flex -jcenter"><img  alt="Product Image" className="menu__left-img -all" /></div>
+          <div className="nav__menu -flex" aria-expanded={true}>
+            <button className="navBar__close -navList-btn -btn-primary">
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"
+                />
+              </svg>
+            </button>
+            <div className="menu__left -flex -jcenter">
+              <img alt="Product Image" className="menu__left-img -all" />
+            </div>
             <div className="menu__right">
               <h4 className="menu__right-title">OUR WHISKEY</h4>
               <ul className="menu__list">
