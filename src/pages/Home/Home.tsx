bc9cc4e95449e4095a1a21fb731282322b705d3c
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-12-10 09:34:09 -0500

    Feat: Cocktail Section - Presentational - completed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-import React,{FC} from "react";
+import React, { FC } from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import "./style.css";
 
-const Home:FC = () => {
+const Home: FC = () => {
   return (
     <>
       <section className="home__presentation -flex -acenter">
@@ -21,7 +21,7 @@ const Home:FC = () => {
         <div className="spire__container -flex">
           <div className="spire__left -flexcol -acenter ">
             <div className="spire__img_reference">
-              <img className="spire__img" alt="spire"/>
+              <img className="spire__img" alt="spire" />
             </div>
             <p className="spire__text">In-Bottle Finishing</p>
             <img alt="decoration Star" className="spire__img-deco" />
@@ -35,23 +35,47 @@ const Home:FC = () => {
                 </span>
                 <span className="spire__right__title">SPIRE</span>
               </div>
-              <p className="spire__right__text-big">AGED in TRADITION. STEEPED in INNOVATION.</p>
-            <p className="spire__right__text-small">At Oak & Eden, we place a 5” long spiral cut piece of wood into every bottle of our fully aged whiskey.</p>
-            <button className="nav__buy-btn -btn-spire -btn-primary">
-            LEARN MORE
-          </button>
+              <p className="spire__right__text-big">
+                AGED in TRADITION. STEEPED in INNOVATION.
+              </p>
+              <p className="spire__right__text-small">
+                At Oak & Eden, we place a 5” long spiral cut piece of wood into
+                every bottle of our fully aged whiskey.
+              </p>
+              <button className="nav__buy-btn -btn-spire -btn-primary">
+                LEARN MORE
+              </button>
             </div>
-          </div>  
+          </div>
         </div>
         <div className="home__spire-line"></div>
       </section>
-      <section className= "featured">
+      <section className="featured">
         <div className="featured__title -flex">
           <span className="featured__title-text -flex -acenter">FEATURED</span>
-          <img alt= "black Star" className="featured__title-icon"></img>
+          <img alt="black Star" className="featured__title-icon"></img>
           <span className="featured__title-text -flex -acenter">FLAVORS</span>
         </div>
-        <Carousel/>
+        <Carousel />
+      </section>
+      <section className="cocktails -flex -acenter -jcenter">
+        <div className="cocktails__container -flexcol -acenter">
+          <div className="cocktails__title -flex -acenter">
+            <img className="cocktails__icon"></img>
+            <h4 className="cocktails__title-text">COCKTAILS</h4>
+            <img className="cocktails__icon"></img>
+          </div>
+          <p className="cocktails__subtitle">
+            Crafted right from the comfort of home sweet home.
+          </p>
+          <p className="cocktails__subtitle-small">
+            Ease back with a cocktail created by some of the best mixologists in
+            the game.
+          </p>
+          <button className="nav__buy-btn -btn-cocktails -btn-primary">
+            SEE ALL RECIPES
+          </button>
+        </div>
       </section>
     </>
   );
