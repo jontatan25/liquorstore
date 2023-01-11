28d8bc09c0f0660a59167039db3bfd64155b4e5e
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-01-11 02:33:36 -0500

    Feat: Accordion Onclick and Animation Implemented.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./accordion.css";
 
 type Props = {
@@ -18,23 +18,36 @@ type Category = {
 };
 
 const Accordion: FC<Props> = ({ products, categories, filterCategory }) => {
+  const [selected, setSelected] = useState<number | null>(null);
+  const handleToogle = (i: number) => {
+    if (selected === i) return setSelected(null);
+    setSelected(i);
+  };
   return (
     <div className="w1__container -flex">
       <div className="w1__left">
         <h3 className="w1__title">{categories[0].name}</h3>
         <p className="w1__description">{categories[0].description}</p>
         <div className="w1__accordion-wrapper">
-          {products.filter((product)=> product.category == filterCategory).map((filteredProduct) => (
-            <>
-              <button className="w1__accordion-title -flex -acenter -btn-primary">
-                <h2> {filteredProduct.name}</h2>
-                <span></span>
-              </button>
-              <div className="w1__accordion-content">
-                {filteredProduct.description}
-              </div>
-            </>
-          ))}
+          {products
+            .filter((product) => product.category == filterCategory)
+            .map((filteredProduct, i) => (
+              <>
+                <button
+                  className="w1__accordion-title -flex -acenter -btn-primary"
+                  onClick={() => handleToogle(i)}
+                >
+                  <h2> {filteredProduct.name}</h2>
+                  <span></span>
+                </button>
+                <div
+                  className="w1__accordion-content"
+                  aria-expanded={selected === i ? true : false}
+                >
+                  {filteredProduct.description}
+                </div>
+              </>
+            ))}
         </div>
       </div>
       <div className="w1__right">hi</div>
